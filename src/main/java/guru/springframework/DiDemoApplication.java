770710477ab6ba4ea45538f8a3f96f762f6f539c
Charles Miller <Charles.Miller@kratosdefense.com>
2018-08-09 10:31:40 -0400

    Add config files

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -7,7 +7,9 @@ import guru.springframework.controllers.PropertyInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"guru.services", "guru.springframework"})
 @SpringBootApplication
 public class DiDemoApplication {
 
